608864db8190315a578858f7e06a3872630ba155
William L. Thomson Jr <wlt@o-sinc.com>
2022-03-12 02:08:17 -0500

    exp/attributes_combos.py: Attributes experimental wip

---
+++ exp/attributes_combos.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import re
+
+def main():
+
+#    filename = input()
+#    if filename == None:
+#       print("filename missing, please pass a filename")
+    filename = "../files/attributes1.txt"
+    attributes = {}
+    count = 0
+
+    try:
+        file = open(filename, "r")
+    except IOError:
+        print ("The file %s was not found, aborting." % filename)
+        exit()
+
+    for line in file:
+        if line[0] == '\n' or line[0] == '#':
+            continue
+        values = re.split(r'[:,]', line[:-1])
+        attributes[values[0]] = (values[1], values[2])
+        print(values)
+        count = count + 1
+
+
+    count = count**2
+    print(attributes)
+    print(count)
+
+    for i in range(0,count):
+        binary = format(i,'b').zfill(4)
+        print(binary)
+        for j in range(0,4):
+            print(list(list(attributes.items())[j])[1][int(binary[j])])
+
+
+if __name__ == "__main__":
+    main()
