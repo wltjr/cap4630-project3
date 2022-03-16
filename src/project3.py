4987742ef277307d9b6ea09299b221f7ab1f30b8
William L. Thomson Jr <wlt@o-sinc.com>
2022-03-16 03:19:22 -0400

    src/project3.py: Make UI layout square vs rectangle

--- src/project3.py
+++ src/project3.py
@@ -15,10 +15,29 @@ if __name__ == '__main__':
     root = Tk()
     root.title('Project 3')
 
-    attributes = Attributes(root)
-    constraints = HardConstraints(root)
-    preferences = Preferences(root)
+    # wrapper frames
+    frame = Frame(root)
+    table_frame = Frame(frame)
 
+    # main UI, tables and forms
+    group_frame = Frame(table_frame)
+    attributes = Attributes(group_frame)
+    group_frame.pack(side=LEFT, padx=5, pady=5)
+    constraints = HardConstraints(group_frame)
+    group_frame.pack(side=LEFT, padx=5, pady=5)
+    table_frame.grid(row=0,column=0)
+    preferences = Preferences(table_frame)
+    table_frame.grid(row=0,column=1)
+    frame.pack()
+
+    # quit button
     quit = Button(root, text='Quit', command=root.quit)
-    quit.pack(side=LEFT, padx=5, pady=5)
+    quit.pack()
     root.mainloop()
+
+    # data structures from classes
+#    attributes.attributes
+#    constraints.constraints
+#    preferences.penalties
+#    preferences.possibilistics
+#    preferences.qualitatives
