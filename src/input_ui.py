db5081ec401a0b596b2572979393eb74b2433f29
William L. Thomson Jr <wlt@o-sinc.com>
2022-03-23 00:13:49 -0400

    src/input_ui.py: Initial abstract base class for input ui classes
    - Implements an initial open file dialog method with error handling and a
      single abstract method for inheriting classes to extend

---
+++ src/input_ui.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from abc import ABC, abstractmethod
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+
+class InputUI(ABC):
+    """
+    InputUI class is a base class with abstract methods for other class to
+    extend and implement 
+    """
+
+    def openFile(self):
+        """
+        Open file selection dialog, base implementation to be called and
+        extended by base classes
+        """
+        filename = fd.askopenfilename()
+
+        if filename == () or filename == "":
+            return
+
+        try:
+            file = open(filename, "r")
+        except IOError:
+            mb.showerror("Error File Not Found",
+                         "The file %s was not found, aborting." % filename)
+            return
+
+        self.filename = filename
+        return file
+
+    @abstractmethod
+    def openFileCallback(self):
+        pass
